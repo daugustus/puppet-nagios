bdc229e11c0725c7b5c8fb7ff23c408f04573d47
Dave Augustus <dave.augustus@bottlerocketapps.com>
2014-08-28 18:14:49 +0000

    updated check_memory

--- manifests/client/brplugins.pp
+++ manifests/client/brplugins.pp
@@ -3,9 +3,9 @@ class nagios::client::brplugins{
 	package{"bc":
 		ensure	=> 	present
 	}
-	file{"/usr/lib64/nagios/plugins/check_memory.sh":
+	file{"/usr/lib64/nagios/plugins/check_memory":
 		ensure	=>	"present",
-		source	=>	"puppet:///modules/nagios/check_memory.sh",
+		source	=>	"puppet:///modules/nagios/check_memory",
 		owner		=>	"root",
 		group		=>	"root",
 		mode		=>	"775",
