0f900eb8f06924c0f2785cdfdf9a98d986730f8a
dave.augustus <dave.augustus@puppet.bottlerocketapps.com>
2014-09-03 15:57:52 +0000

    added nrpe command template

---
+++ manifests/nrpecommand.pp
@@ -0,0 +1,21 @@
+define nagios::nrpecommand(
+	$args						=	undef,
+	$command				=	$name,
+	$ensure					=	undef,
+	$host						=	"${::fqdn}",
+	$nagios_conf_d	=	$nagios::params::nagios_confd,
+	$nrpe_template	=	'check_command',
+	$plugin					=	$name,
+	$sudo						=	false,
+	$sudo_user			=	undef,
+){
+
+	file { "${nagios::params::nrpe_cfg_dir}/nrpe-${title}.cfg":
+    content => template("nagios/nrpe-${nrpe_template}.cfg.erb"),
+    ensure  => $ensure,
+    group   => $nagios::client::nrpe_group,
+    mode    => '0640',
+    notify  => Service['nrpe'],
+  	owner   => 'root',
+  }	
+}
