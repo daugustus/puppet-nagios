619425e5993928f8495961b4d28caa3530b988e5
dave.augustus <dave.augustus@puppet.bottlerocketapps.com>
2014-09-03 21:45:33 +0000

    added hostgroup from environment settings

--- manifests/export.pp
+++ manifests/export.pp
@@ -1,9 +1,14 @@
 class nagios::export{
+
+	$settings = hiera(settings, {})
+	if $settings[nagios_hostgroup] {
+		$host_groups = ${settings[nagios_hostgroup]}
+
 	@@nagios_host { "${::fqdn}" :
     address             => $::ipaddress,
     check_command       => 'check-host-alive',
   	ensure              => present,
-    hostgroups          => 'all-servers',
+    hostgroups          => "all-servers,${host_groups}",
     target              => "/etc/nagios/conf.d/host_${::fqdn}.cfg",
     use                 => 'host-alarm-l9,host-check-l9',
   }
